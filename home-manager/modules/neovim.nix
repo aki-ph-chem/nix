f7bff8809c94a6c6e6a9de380b814b80d087b98a
aki <aki@mymail.com>
2025-10-19 06:06:35 +0900

    I've set it up to pass environment variables to Neovim.

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -21,12 +21,13 @@ in
     pkgs.libskk
   ];
 
-  home.sessionVariables = {
-    SKK_JISYO_L_PATH = traceSkkDictPath;
-  };
-
   programs.neovim = {
     enable = true;
+
+    environment = {
+      SKK_JISYO_L_PATH = traceSkkDictPath;
+    };
+
     extraPackages = [
       # Language Servers
       pkgs.gopls
