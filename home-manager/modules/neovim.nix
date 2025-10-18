b392fbe53683972bb018f4cda64cf3300d04528e
aki <aki@mymail.com>
2025-10-19 05:54:40 +0900

    I set the path to SKK-JISYO.L included in libskk installed via home-manager as an environment variable.

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -6,12 +6,23 @@
 
 let
   nvimConfigPath = "${config.home.homeDirectory}/neovim-config/nvim";
+  skkDict = pkgs.libskk;
+  skkDictPath = "${skkDict}/share/skk/SKK-JISYO.L";
+  traceSkkDictPath = builtins.trace ("DEBUG: skkDictPath " + skkDictPath) skkDictPath;
 in
 {
 
   home.file.".config/nvim" = {
     source = config.lib.file.mkOutOfStoreSymlink "${nvimConfigPath}";
     recursive = true;
+  };
+
+  home.packages = [
+    pkgs.libskk
+  ];
+
+  home.sessionVariables = {
+    SKK_JISYO_L_PATH = traceSkkDictPath;
   };
 
   programs.neovim = {
