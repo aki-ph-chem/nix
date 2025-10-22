808d56e701a6720a9db92f6742a1faa865b73c05
aki <aki@mymail.com>
2025-10-22 12:12:44 +0900

    Configuration for using Neovim nightly: ok

--- flake.nix
+++ flake.nix
@@ -8,13 +8,23 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # use neovim nightly: see https://github.com/nix-community/neovim-nightly-overlay
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      nixpkgs,
+      home-manager,
+      neovim-nightly-overlay,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      overlays = [
+        neovim-nightly-overlay.overlays.default
+      ];
     in
     {
       homeConfigurations."aki" = home-manager.lib.homeManagerConfiguration {
@@ -22,7 +32,10 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home-manager/home.nix ];
+        modules = [
+          ./home-manager/home.nix
+          { nixpkgs.overlays = overlays; }
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
