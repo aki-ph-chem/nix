6068525996f507518e30b5129defd04603c331bb
aki <aki@mymail.com>
2025-10-24 03:22:00 +0900

    add qpdfview #18

--- home-manager/modules/gui-app.nix
+++ home-manager/modules/gui-app.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
+let
+  qpdfvieWrapper = pkgs.writeShellScriptBin "qpdfview" ''
+    exec ${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa  ${pkgs.qpdfview}/bin/qpdfview "$@" 
+  '';
+in
 {
 
   home.packages = [
     pkgs.nixgl.nixGLMesa
+    qpdfvieWrapper
   ];
 }
