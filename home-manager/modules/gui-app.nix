ef5483f53514083a1b1276b211c88f9ebeb95262
aki <aki@mymail.com>
2025-10-29 23:25:31 +0900

    fix: fix desktop entry for qpdfview

--- home-manager/modules/gui-app.nix
+++ home-manager/modules/gui-app.nix
@@ -10,6 +10,13 @@ in
     name = "qpdfview";
     exec = "${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa  ${pkgs.qpdfview}/bin/qpdfview";
     icon = "${pkgs.qpdfview}/share/icons/hicolor/scalable/apps/qpdfview.svg";
+    terminal = false;
+    genericName = "tabbed document viewer";
+    comment = "A tabbed document viewer using Qt and the Poppler library.";
+    categories = [
+      "Viewer"
+      "Office"
+    ];
   };
 
   home.packages = [
