9b4b400fa280b7fdd87ea7ffd96376fc1f87f630
aki <aki@mymail.com>
2025-10-29 23:10:27 +0900

    feat: add desktop entry for zathura

--- home-manager/modules/gui-app.nix
+++ home-manager/modules/gui-app.nix
@@ -13,6 +13,25 @@ in
     name = "qpdfview";
     exec = "${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa  ${pkgs.qpdfview}/bin/qpdfview";
   };
+  xdg.desktopEntries."zathura.desktop" = {
+    type = "Application";
+    name = "Zathura";
+    comment = "A minimalistic document viewer";
+    terminal = false;
+    noDisplay = false;
+    categories = [
+      "Office"
+      "Viewer"
+    ];
+    mimeType = [
+      "application/pdf"
+      "application/oxps"
+      "application/epub+zip"
+      "application/x-fictionbook"
+    ];
+    exec = "${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa  ${pkgs.zathura}/bin/zathura";
+    icon = "${pkgs.zathura}/share/icons/hicolor/scalable/apps/org.pwmt.zathura.svg";
+  };
 
   home.packages = [
     pkgs.nixgl.nixGLMesa
