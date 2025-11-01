d8e3fd0d0cc0f33b5feccc5dd08730eef189b5a1
aki <aki@mymail.com>
2025-11-01 19:41:27 +0900

    refactor: refactored the code around skk.

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -6,14 +6,16 @@
 
 let
   nvimConfigPath = "${config.home.homeDirectory}/neovim-config/nvim";
-  skkDict = pkgs.libskk;
-  skkDictPath = "${skkDict}/share/skk/SKK-JISYO.L";
-  traceSkkDictPath = builtins.trace ("DEBUG: skkDictPath " + skkDictPath) skkDictPath;
+  libSkk = pkgs.libskk;
+  # skk dict
+  skkDictPath = "${libSkk}/share/skk/SKK-JISYO.L";
+  # skk emoji dict ja
   skkEmojiDictJa = builtins.fetchTarball {
     url = "https://github.com/ymrl/SKK-JISYO.emoji-ja/archive/refs/heads/master.tar.gz";
     sha256 = "1mws99wxy5j55aa3dv8ynqqbx5iw4553vkda6rsh054yn8hiy1bb";
   };
   skkEmojiDictJaPath = "${skkEmojiDictJa}/SKK-JISYO.emoji-ja.utf8";
+  # skk emoji dict en
   skkEmojiDict = builtins.fetchTarball {
     url = "https://github.com/uasi/skk-emoji-jisyo/archive/refs/heads/master.tar.gz";
     sha256 = "0pg4cgc5qhal2g38wdrzfhpxxa2iabbrn09chhhkj258w2h2xw6b";
@@ -37,7 +39,7 @@ in
     extraWrapperArgs = [
       "--set"
       "SKK_JISYO_L_PATH"
-      traceSkkDictPath
+      skkDictPath
       "--set"
       "SKK_JISYO_EMOJI_JA_PATH"
       skkEmojiDictJaPath
