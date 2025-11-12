39d3808f47cf9abf390478593b42e9c8f990eaab
aki <aki@mymail.com>
2025-11-12 23:47:25 +0900

    feat: Add a desktop entry for neovide

--- home-manager/modules/gui-app.nix
+++ home-manager/modules/gui-app.nix
@@ -75,6 +75,36 @@ in
     icon = "${pkgs.xfce.ristretto}/share/icons/hicolor/scalable/apps/org.xfce.ristretto.svg";
   };
 
+  # neovide
+  xdg.desktopEntries."neovide" = {
+    type = "Application";
+    name = "Neovide";
+    comment = "No Nonsense Neovim Client in Rust";
+    categories = [
+      "Utility"
+      "TextEditor"
+    ];
+    mimeType = [
+      "text/english"
+      "text/plain"
+      "text/x-makefile"
+      "text/x-c++hdr"
+      "text/x-c++src"
+      "text/x-chdr"
+      "text/x-csrc"
+      "text/x-java"
+      "text/x-moc"
+      "text/x-pascal"
+      "text/x-tcl"
+      "text/x-tex"
+      "application/x-shellscript"
+      "text/x-c"
+      "text/x-c++"
+    ];
+    exec = "${nixGl} ${pkgs.neovide}/bin/neovide";
+    icon = "${pkgs.neovide}/share/icons/hicolor/scalable/apps/neovide.svg";
+  };
+
   home.packages = [
     pkgs.nixgl.nixGLMesa
     qpdfvieWrapper
