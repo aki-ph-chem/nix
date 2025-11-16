e682f822bf0983881d319657b53fad8e34a54936
aki <aki@mymail.com>
2025-11-16 16:12:29 +0900

    feat: add `user.signingkey`

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -6,6 +6,7 @@
     settings = {
       user.name = "aki";
       user.email = "aki@mymail.com";
+      user.signingkey = "9EFC159FE516A29E3AAD1E32608634461D56BEA9";
       extraConfig.init = {
         defaultBranch = "main";
       };
