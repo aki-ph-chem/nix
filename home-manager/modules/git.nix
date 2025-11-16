6c77829691b4318a616e054a53d95cee5d4dcf25
aki <lmxox@proton.me>
2025-11-16 16:35:17 +0900

    feat: Updated the `settings.user.email` in `home-manager/modules/git.nix` to match the PGP email address.

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       user.name = "aki";
-      user.email = "aki@mymail.com";
+      user.email = "lmxox@proton.me";
       user.signingkey = "9EFC159FE516A29E3AAD1E32608634461D56BEA9";
       extraConfig.init = {
         defaultBranch = "main";
