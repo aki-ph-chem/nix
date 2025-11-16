70d012611afa36cd1d5ea30938dfcc5de140c4c3
aki <lmxox@proton.me>
2025-11-16 16:49:37 +0900

    feat: add `commit.gpgsign = true;` to `home-manager/modules/git.nix`

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -7,6 +7,7 @@
       user.name = "aki";
       user.email = "lmxox@proton.me";
       user.signingkey = "9EFC159FE516A29E3AAD1E32608634461D56BEA9";
+      commit.gpgsign = true;
       extraConfig.init = {
         defaultBranch = "main";
       };
