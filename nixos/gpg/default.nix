c6ec6985dbd6d78e6afd38fa571170cd7b5207ad
aki <lmxox@proton.me>
2025-11-30 14:31:33 +0900

    feat: config for yubikey & gpg => OK

--- nixos/gpg/default.nix
+++ nixos/gpg/default.nix
@@ -1,16 +1,17 @@
 { pkgs, userName }:
 {
 
-  users.users."${userName}" = {
-    packages = with pkgs; [
-      gnupg
-    ];
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      enableExtraSocket = true;
+      # use pinentry-rofi
+      pinentryPackage = pkgs.pinentry-rofi;
+    };
   };
 
-  # for smart card like Yubikey
+  # for Yubikey
   services.pcscd.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableExtraSocket = true;
-  };
+  programs.yubikey-manager.enable = true;
+  services.yubikey-agent.enable = true;
 }
