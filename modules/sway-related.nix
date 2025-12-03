2b784d53f350ba5ec1c0f74bf4969c0d2f08461d
aki <lmxox@proton.me>
2025-12-03 22:59:38 +0900

    refactor: refactor sway config for Arch Linux + home-manager

--- modules/sway-related.nix
+++ modules/sway-related.nix
@@ -4,7 +4,9 @@
   dotfiles,
   ...
 }:
-
+let
+  swayConfigBase = builtins.readFile "${dotfiles}/sway/config";
+in
 {
   home.packages = [
     # sway itself is cannot managed by Nix on Arch Linux
@@ -18,7 +20,32 @@
   home.file = {
     # sway
     ".config/sway/config" = {
-      source = "${dotfiles}/sway/config";
+      text = ''
+        ${swayConfigBase}
+        # ==========================================================
+        # === START: Configuration Added/Merged by Home Manager  ===
+        # ==========================================================
+
+        # workspace 1 => fixed to eDP-1
+        workspace 1 output eDP-1
+
+        # swayfx specific
+        corner_radius 10
+
+        ## how to exit sway session
+        #
+        # $ swaymsg exit
+        #
+        # systemd
+        exec_always "systemctl --user start sway-session.target"
+
+        # for polkit
+        exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+
+        # ========================================================
+        # === END: Configuration Added/Merged by Home Manager  ===
+        # ========================================================
+      '';
     };
     # waybar
     ".config/waybar" = {
