af1102de9d44b4ae489bccae876c1a6a5250bdd7
aki <lmxox@proton.me>
2025-12-06 00:33:32 +0900

    feat: GUI applications in the FHS environment that depend on GTK2 can now be executed.

--- nixos/nix-ld/default.nix
+++ nixos/nix-ld/default.nix
@@ -1,5 +1,33 @@
 { pkgs }:
 {
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # X11
+      xorg.libX11
+      xorg.libXext
+      xorg.libXrender
+      xorg.libXrandr
+      xorg.libXcursor
+      xorg.libXcomposite
+      xorg.libXtst
+      xorg.libXfixes
+      xorg.libxcb
+      xorg.libXdamage
+      xorg.libxshmfence
+      xorg.libXxf86vm
 
+      # GTK2
+      glib
+      gtk2
+      gdk-pixbuf
+
+      # others
+      zlib
+      pango
+      atk
+      cairo
+      stdenv.cc.cc
+    ];
+  };
 }
