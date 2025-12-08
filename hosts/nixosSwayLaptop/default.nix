5a0143f808db07867f6d8201035e2f0aeb6a61bf
aki <lmxox@proton.me>
2025-12-08 18:45:29 +0900

    fix: separate locale config

--- hosts/nixosSwayLaptop/default.nix
+++ hosts/nixosSwayLaptop/default.nix
@@ -29,6 +29,7 @@ nixpkgs.lib.nixosSystem {
         (import "${flakeRoot}/nixos/polkit" { inherit pkgs; })
         (import "${flakeRoot}/nixos/gui-app" { inherit pkgs userName; })
         (import "${flakeRoot}/nixos/nix-ld" { inherit pkgs; })
+        (import "${flakeRoot}/nixos/locale" { inherit pkgs; })
         # Include the results of the hardware scan.
         ./hardware-configuration.nix
         home-manager.nixosModules.home-manager
@@ -97,21 +98,6 @@ nixpkgs.lib.nixosSystem {
       # Set your time zone.
       time.timeZone = "Asia/Tokyo";
 
-      # Select internationalisation properties.
-      i18n = {
-        defaultLocale = "en_US.UTF-8";
-        extraLocaleSettings = {
-          LC_ADDRESS = "ja_JP.UTF-8";
-          LC_IDENTIFICATION = "ja_JP.UTF-8";
-          LC_MEASUREMENT = "ja_JP.UTF-8";
-          LC_MONETARY = "ja_JP.UTF-8";
-          LC_NAME = "ja_JP.UTF-8";
-          LC_NUMERIC = "ja_JP.UTF-8";
-          LC_PAPER = "ja_JP.UTF-8";
-          LC_TELEPHONE = "ja_JP.UTF-8";
-          LC_TIME = "ja_JP.UTF-8";
-        };
-      };
       console = {
         font = "Lat2-Terminus16";
         keyMap = "us";
