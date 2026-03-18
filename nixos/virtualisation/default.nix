c28f23b0a6a7c537a42194031b7119abc6324338
aki <lmxox@proton.me>
2026-03-18 21:13:19 +0900

    feat/containerd-wasmtime-shim: remove unused code

--- nixos/virtualisation/default.nix
+++ nixos/virtualisation/default.nix
@@ -10,12 +10,6 @@
     # cotainerd
     containerd = {
       enable = true;
-      settings = {
-        ## config for containerd-shim-wasmtime-v1
-        plugins."io.containerd.grpc.v1.cri".containerd.runtimes.wasm = {
-          runtime_type = "io.containerd.wasmtime.v1";
-        };
-      };
     };
 
     # QEMU/KVM by libvirt
