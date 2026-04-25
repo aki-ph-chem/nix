13653eb0c6f1c7b72e4308ad28124bed5341d334
aki <lmxox@proton.me>
2026-04-26 00:04:28 +0900

    feat(libinput-gestures): add libinput as service

--- nixos/sway/default.nix
+++ nixos/sway/default.nix
@@ -29,9 +29,11 @@
 
     serviceConfig = {
       Type = "simple";
-      ExecStart = ''${pkgs.kanshi}/bin/kanshi'';
+      ExecStart = "${pkgs.kanshi}/bin/kanshi";
     };
   };
+
+  services.libinput.enable = true;
 
   users.users."${userName}".packages = with pkgs; [
     ## sway-related
