13f9327938de3c4ed45835f112b8e833979f7375
aki <lmxox@proton.me>
2026-05-19 22:19:40 +0900

    fix: Changed the method for fetching skk-emoji-jisyo to `pkgs.fetchFromGitHub`

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -23,9 +23,11 @@ let
   };
   skkEmojiDictJaPath = "${skkEmojiDictJa}/SKK-JISYO.emoji-ja.utf8";
   # skk emoji dict en
-  skkEmojiDict = builtins.fetchTarball {
-    url = "https://github.com/uasi/skk-emoji-jisyo/archive/refs/heads/master.tar.gz";
-    sha256 = "0pg4cgc5qhal2g38wdrzfhpxxa2iabbrn09chhhkj258w2h2xw6b";
+  skkEmojiDict = pkgs.fetchFromGitHub {
+    owner = "uasi";
+    repo = "skk-emoji-jisyo";
+    rev = "18ff911d0dc445dbc8fbc4c5122874d08e56781b";
+    hash = "sha256-y/AuoOCoCDkhhCwBm9dSUajeL3Q/N47GE1RBXNhj5F0=";
   };
   skkEmojiDictPath = "${skkEmojiDict}/SKK-JISYO.emoji.utf8";
 in
