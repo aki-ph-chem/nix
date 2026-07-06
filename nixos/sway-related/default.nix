81ab1f5b45d131e424ebdb43f576b4bb7746fa81
aki <lmxox@proton.me>
2026-07-07 07:04:33 +0900

    feat: define waybar-niri.service(wip)

--- nixos/sway-related/default.nix
+++ nixos/sway-related/default.nix
@@ -10,4 +10,25 @@
     flameshot
     libinput-gestures
   ];
+  systemd = {
+    # disabled waybar.services
+    user.services.waybar.wantedBy = pkgs.lib.mkForce [ ];
+
+    # waybar for niri
+    user.services.waybar-niri = {
+      # [Unit]
+      description = "Highly customizable Wayland bar for Sway and Wlroots based compositors";
+      documentation = "https://github.com/Alexays/Waybar/wiki/";
+      partOf = "graphical-session.target";
+      after = "graphical-session.target";
+      requisite = "graphical-session.target";
+
+      #[Services]
+      # wip
+      ## ToDo: how to get $HOME
+
+      #[Install]
+      wantedBy = [ "niri-session.target" ];
+    };
+  };
 }
